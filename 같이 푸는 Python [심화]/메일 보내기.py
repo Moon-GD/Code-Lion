4cf90b5458d63038ba138afa5ee30b6b40af74a0
Moon GD_LG <king4mun@gmail.com>
2022-01-21 13:02:55 +0900

    같이 푸는 Python 심화 메일 보내기 완강

--- 같이 푸는 Python [심화]/메일 보내기.py
+++ 같이 푸는 Python [심화]/메일 보내기.py
@@ -1,27 +1,52 @@
-# 사전 준비 :
-# 1. 계정 imap 활성화
-# 2. 계정 외부 활동의 접근 허용
-
+import re
 import smtplib
-from email.message import EmailMessage  # MIME 기능을 사용하기 위한 모듈, 기능
+from email.message import EmailMessage
+import imghdr
 
+# smtp 연결을 위한 정보
 SMTP_SERVER = "smtp.gmail.com"
-# 지메일 포트 번호 : 465
 SMTP_PORT = 465
 
-message = EmailMessage()
-message.set_content("email.message 모듈의 EmailMessage 기능을 사용하여 작성한 본문입니다.")
-
-# SMTP 연결하기
-
-## 일반적으로는 SMTP 방식으로 서버와 연결하면 되지만
-## 지메일 같이 SSL 방식으로 암호화된 서버는 SMTP_SSL을 통해 추가적인 정보를 제공해야 한다.
+# SSL을 통한 smtp 연결
 smtp = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
 
-# SMTP 로그인하기
-id = "king4mun@likelion.org"
-password = "6f7472mun"
-smtp.login(id, password=password)
+# 로그인 정보
+mail_id = "king4mun@likelion.org"
+mail_pw = "6f7472mun"
 
-smtp.send_message()
+# smtp 객체를 통해 지메일 로그인
+smtp.login(mail_id, password=mail_pw)
+
+# 메일 표준 방식인 MIME을 사용하는 EmailMessage 객체 생성
+message = EmailMessage()
+
+# emailmessage 객체 해더 작성
+message["Subject"] = "제목 입니다. (최종)"
+message["From"] = mail_id
+message["To"] = mail_id
+
+# emailmessage 본문 작성
+message.set_content("본문 내용입니다. (최종) 요새 열정이 다시 조금씩 불타는 중")
+
+# 이미지 읽어오기
+with open("logo.png", "rb") as image:
+    image_file = image.read()
+
+# 혹시 모를 이미지 확장자 변환을 대비하여 유연하게 subtype 지정
+image_type = imghdr.what("logo", image_file)
+
+# image file 첨부
+message.add_attachment(image_file, maintype="image", subtype=image_type)
+
+# 수신인 메일 유효성 체크
+reg = "^[a-zA-Z0-9.-_+]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]{2,3}$"
+isOk = re.match(reg, message["To"])
+
+if bool(isOk):
+    smtp.send_message(message)
+    print("정상적으로 전송되었습니다.")
+else:
+    print("유효하지 않은 이메일 주소입니다.")
+
+# smtp 연결 해제
 smtp.quit()
---
+++ 같이 푸는 Python [심화]/연습 공간.py
@@ -0,0 +1,52 @@
+import re
+import smtplib
+from email.message import EmailMessage
+import imghdr
+
+# smtp 연결을 위한 정보
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 465
+
+# SSL을 통한 smtp 연결
+smtp = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
+
+# 로그인 정보
+mail_id = "king4mun@likelion.org"
+mail_pw = "6f7472mun"
+
+# smtp 객체를 통해 지메일 로그인
+smtp.login(mail_id, password=mail_pw)
+
+# 메일 표준 방식인 MIME을 사용하는 EmailMessage 객체 생성
+message = EmailMessage()
+
+# emailmessage 객체 해더 작성
+message["Subject"] = "제목 입니다. (최종)"
+message["From"] = mail_id
+message["To"] = mail_id
+
+# emailmessage 본문 작성
+message.set_content("본문 내용입니다. (최종) 요새 열정이 다시 조금씩 불타는 중")
+
+# 이미지 읽어오기
+with open("logo.png", "rb") as image:
+    image_file = image.read()
+
+# 혹시 모를 이미지 확장자 변환을 대비하여 유연하게 subtype 지정
+image_type = imghdr.what("logo", image_file)
+
+# image file 첨부
+message.add_attachment(image_file, maintype="image", subtype=image_type)
+
+# 수신인 메일 유효성 체크
+reg = "^[a-zA-Z0-9.-_+]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]{2,3}$"
+isOk = re.match(reg, message["To"])
+
+if bool(isOk):
+    smtp.send_message(message)
+    print("정상적으로 전송되었습니다.")
+else:
+    print("유효하지 않은 이메일 주소입니다.")
+
+# smtp 연결 해제
+smtp.quit()
