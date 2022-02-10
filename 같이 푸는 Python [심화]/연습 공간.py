9c5e5ab8fa673ff3b86d4680a7846a50dc630106
Moon GD_LG <king4mun@gmail.com>
2022-02-10 17:23:21 +0900

    일단 만드는 django 시작 및 개발 환경 구축 완료

--- 같이 푸는 Python [심화]/연습 공간.py
+++ 같이 푸는 Python [심화]/연습 공간.py
@@ -1,52 +1,18 @@
-import re  # 정규 표현식 사용을 위한 모듈
-import smtplib  # smtp 연결을 위한 모듈
-from email.message import EmailMessage  # MIME 메일 방식을 사용하기 위한 모듈
-import imghdr  # 이미지 확장자를 읽어오기 위한 모듈
+import requests
+from bs4 import BeautifulSoup
+import datetime
 
-# smtp 연결을 위한 정보
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 465
+year = datetime.datetime.today().year
+month = datetime.datetime.today().month
+day = datetime.datetime.today().day
 
-# SSL을 통한 smtp 연결
-smtp = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
+print(f'{year}년 {month}월 {day}일 각 지역 온도입니다.', end="\n\n")
+city_list = ['서울', '대전', '보성', '대구']
+for city in city_list:
+    url = 'https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q=' + city + '+날씨'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    temp_list = soup.findAll('strong', class_='txt_temp')
+    temp = temp_list[0].text
 
-# 로그인 정보
-mail_id = "king4mun@likelion.org"
-mail_pw = "6f7472mun"
-
-# smtp 객체를 통해 지메일 로그인
-smtp.login(mail_id, password=mail_pw)
-
-# 메일 표준 방식인 MIME을 사용하는 EmailMessage 객체 생성
-message = EmailMessage()
-
-# emailmessage 객체 해더 작성
-message["Subject"] = "제목 입니다. (최종)"
-message["From"] = mail_id
-message["To"] = mail_id
-
-# emailmessage 본문 작성
-message.set_content("본문 내용입니다. (최종) 요새 열정이 다시 조금씩 불타는 중")
-
-# 이미지 읽어오기
-with open("logo.png", "rb") as image:
-    image_file = image.read()
-
-# 혹시 모를 이미지 확장자 변환을 대비하여 유연하게 subtype 지정
-image_type = imghdr.what("logo", image_file)
-
-# image file 첨부
-message.add_attachment(image_file, maintype="image", subtype=image_type)
-
-# 수신인 메일 유효성 체크
-reg = "^[a-zA-Z0-9.-_+]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]{2,3}$"
-isOk = re.match(reg, message["To"])
-
-if bool(isOk):
-    smtp.send_message(message)
-    print("정상적으로 전송되었습니다.")
-else:
-    print("유효하지 않은 이메일 주소입니다.")
-
-# smtp 연결 해제
-smtp.quit()
+    print(f'{city}의 온도는 {temp} 입니다.')
